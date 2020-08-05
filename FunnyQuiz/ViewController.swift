d71af3fadea51b34439ef0be3b062fdac837f813
Alexander <hello@alexander.works>
2020-08-05 22:22:39 +0300

    Built general interface

--- FunnyQuiz/ViewController.swift
+++ FunnyQuiz/ViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var questionLbl: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        let headlineStyle = NSMutableParagraphStyle()
+//        headlineStyle.lineSpacing = 1
+//
+//        let attrString = NSMutableAttributedString(string: "Question text goes here")
+//        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value:headlineStyle, range:NSMakeRange(0, attrString.length))
+//
+//        question.attributedText = NSAttributedString(attributedString: attrString)
     }
 
 
