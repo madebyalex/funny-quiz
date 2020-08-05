3d7bf981eb355b2755b774ec58b321678401c87c
Alexander <hello@alexander.works>
2020-08-05 19:10:21 +0300

    Initial Commit

---
+++ FunnyQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FunnyQuiz
+//
+//  Created by Alexander on 05.08.2020.
+//  Copyright © 2020 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
