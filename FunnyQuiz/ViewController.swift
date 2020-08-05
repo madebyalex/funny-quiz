912ad40daacba17ddedf186f07e4d0056bee20dd
Alexander <hello@alexander.works>
2020-08-06 01:19:32 +0300

    Checking correct answers & Looping through the questions

--- FunnyQuiz/ViewController.swift
+++ FunnyQuiz/ViewController.swift
@@ -11,19 +11,53 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var questionLbl: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueBtn: UIButton!
+    @IBOutlet weak var falseBtn: UIButton!
+    @IBOutlet weak var currentQuestionLbl: UILabel!
+    @IBOutlet weak var totalQuestionsLbl: UILabel!
     
+    let quiz = [
+        ["Pink is the most common colour of toilet paper in France", "True"],
+        ["Scooby Doo’s full name is Scooberdy Doo", "False"],
+        ["You can sneeze in your sleep", "False"]
+    ]
     
+    var questionNumber = 0
+    var totalQuestions: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let headlineStyle = NSMutableParagraphStyle()
-//        headlineStyle.lineSpacing = 1
-//
-//        let attrString = NSMutableAttributedString(string: "Question text goes here")
-//        attrString.addAttribute(NSAttributedString.Key.paragraphStyle, value:headlineStyle, range:NSMakeRange(0, attrString.length))
-//
-//        question.attributedText = NSAttributedString(attributedString: attrString)
+        totalQuestions = quiz.count
+        totalQuestionsLbl.text = String(totalQuestions)
+        updateUI()
     }
 
-
+    @IBAction func answerBtnPressed(_ sender: UIButton) {
+        
+        checkAnswer(sender: sender.currentTitle ?? "")
+        
+        if questionNumber + 1 < totalQuestions {
+            questionNumber += 1
+        } else {
+            print("Game over")
+            questionNumber = 0
+        }
+        
+        updateUI()
+    }
+    
+    func updateUI() {
+        currentQuestionLbl.text = String(questionNumber + 1)
+        questionLbl.text = quiz[questionNumber][0]
+    }
+    
+    func checkAnswer(sender: String) {
+        if quiz[questionNumber][1] == sender {
+            print("Correct!")
+        } else {
+            print("Wrong")
+        }
+    }
+    
 }
 
